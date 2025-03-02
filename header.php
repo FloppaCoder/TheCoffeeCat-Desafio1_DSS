4bf4853fb90d50d931e35fa7daa392b37d7b2ece
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-03-01 18:00:27 -0600

    Agregando Header

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TheCoffeeCat - Desafio 1 DSS</title>
+    <!-- Tailwind css -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Libreria AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!-- Iconos Flaticon -->
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.4.0/uicons-brands/css/uicons-brands.css'>
+    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/2977/2977985.png">
+</head>
+<body class="bg-gray-100"></body>
