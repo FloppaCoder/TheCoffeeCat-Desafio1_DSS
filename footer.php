27fe0fda276ec43ab091337fe076b0a47ee0f07f
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-03-01 19:02:01 -0600

    Adding Footer and Header fixes

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="bg-[#98694d] text-zinc-50 p-3 pb-4">
+    <div class="container mx-auto text-center">
+        <aside>
+        <div class="flex flex-row justify-center items-center font-bold text-3xl">
+                <img src="img/logoSinFondo.png" alt="loguito" class="w-[5rem]">
+                <span>TheCoffeeCat</span>
+            </div>
+            <p class="">
+                Marcelo Menjivar - CM232707 <br>
+                Eleazar Amaya - AS232697
+            </p>
+        </aside>
+    </div>
+</footer>
--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TheCoffeeCat - Desafio 1 DSS</title>
     <!-- Tailwind css -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <!-- Libreria AOS -->
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
     <!-- Iconos Flaticon -->
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.4.0/uicons-brands/css/uicons-brands.css'>
-    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/2977/2977985.png">
+    <link rel="icon" type="image/png" href="img/logoSinFondo.png">
 </head>
-<body class="bg-gray-100"></body>
+
+<body class="bg-gradient-to-t from-stone-900 to-neutral-900"></body>
