4111950dcd6dfc35a04be019ae95f6b025646cff
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-03-01 19:01:04 -0600

    Adding Sidebar to the landing page

---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<aside class="bg-[#98694d] p-6 rounded-lg shadow-lg">
+    <h2 class="text-2xl mb-4 font-bold text-zinc-50">Eventos Especiales</h2>
+    <ul>
+        <li class="mb-3">
+            <a href="#" class="font-bold text-zinc-50 hover:text-zinc-300">Noche de Gatos y Café</a>
+            <p class="text-sm text-zinc-50">Viernes, 20 de octubre - 7:00 PM</p>
+        </li>
+        <li class="mb-3">
+            <a href="#" class="font-bold text-zinc-50 hover:text-zinc-300">Taller de Barismo</a>
+            <p class="text-sm text-zinc-50">Sábado, 28 de octubre - 10:00 AM</p>
+        </li>
+        <li class="mb-3">
+            <a href="#" class="font-bold text-zinc-50 hover:text-zinc-300">Adopción de Gatos</a>
+            <p class="text-sm text-zinc-50">Domingo, 5 de noviembre - 12:00 PM</p>
+        </li>
+    </ul>
+</aside>
