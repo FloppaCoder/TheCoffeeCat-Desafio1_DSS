13fac65d10fb0bbf78207f432216ac22f231e044
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-03-01 18:12:17 -0600

    Agregando menú Navbar

---
+++ nav.php
@@ -0,0 +1,15 @@
+<nav class="bg-[#98694d] ">
+        <ul class="text-zinc-50 text-lg flex flex-row justify-between px-12 py-1">
+
+            <li class="flex flex-row items-center font-bold text-2xl"><img src="img/logoSinFondo.png"
+                    alt="loguito" class="w-[3.5rem]">
+                Floppa Café
+            </li>
+
+            <div class=" flex flex-col md:flex-row items-center md:gap-[5rem] gap-5">
+                <li class="hover:bg-[#553d3a] px-3 py-2 rounded-md">Inicio</li>
+                <li class="hover:bg-[#553d3a] px-3 py-2 rounded-md">Catalogo</li>
+                <li class="hover:bg-[#553d3a] px-3 py-2 rounded-md">Tipos de Café</li>
+            </div>
+        </ul>
+    </nav>
