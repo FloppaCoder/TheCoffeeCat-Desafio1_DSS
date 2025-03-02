e1278549938047396c6633d12d01cacfe98233c3
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-03-01 19:03:22 -0600

    Responsive Fixes

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,5 +10,4 @@
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.4.0/uicons-brands/css/uicons-brands.css'>
     <link rel="icon" type="image/png" href="img/logoSinFondo.png">
 </head>
-
 <body class="bg-gradient-to-t from-stone-900 to-neutral-900"></body>
--- index.php
+++ index.php
@@ -7,11 +7,14 @@
         <!-- /#header y #nav -->
 
         <!-- #content y #sidebar -->
-        <div class="flex flex-1">
+        <div class="flex flex-col lg:flex-row flex-1">
+            <!-- Contenido Principal -->
             <main class="flex-1 p-6">
                 <?php require('content.php'); ?>
             </main>
-            <aside class="w-1/4 p-6">
+
+            <!-- Sidebar (Se moverá debajo del contenido en pantallas pequeñas) -->
+            <aside class="w-full lg:w-1/4 p-6">
                 <?php require('sidebar.php'); ?>
             </aside>
         </div>
